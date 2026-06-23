26c6b2120881194f96fc054c79f96935c85c652a
Samuel Gebing <samuel.gebing@farroupilha.local>
2026-06-23 14:39:35 -0300

    feat(professional): implement error accumulation and try-catch validation

--- src/services/professionalService.js
+++ src/services/professionalService.js
@@ -1,7 +1,54 @@
 const ProfessionalModel = require("../models/professionalModel");
 // Importa o Model responsável pelo acesso ao banco de dados (tabela profissionais)
 
+const validatePhone = require("../utils/validatePhone");
+// Importa a função de validação de telefone do arquivo validatePhone.js
+
 class ProfessionalService {
+    // Valida os dados do profissional antes de criar ou atualizar
+    static validateProfessional(professional) {
+        if (!professional) throw new Error("Profissional não fornecido.");
+
+        const errors = [];
+        // Verifica campos obrigatórios
+        if (!professional.nome) errors.push("Nome do profissional não fornecido.");
+        if (!professional.telefone) errors.push("Telefone do profissional não fornecido.");
+        if (!professional.especialidade_id) errors.push("Especialidade do profissional não fornecida.");
+        
+        if (errors.length > 0) { 
+            errors[0] = "FALHA NA VALIDAÇÃO DO PROFISSIONAL: " + errors[0]; // Prefixa a primeira mensagem de erro
+            throw new Error(errors.join(" ")); // Lança um erro com todas as mensagens de validação
+        }
+        
+        if (!professional.ativo) professional.ativo = 1; 
+        // Define status padrão como 1 (ativo) caso não seja fornecido
+
+        // VALIDAÇÕES DE NOME
+        if (typeof professional.nome !== "string") 
+            errors.push("Nome com formato inválido.");
+        if (professional.nome.length < 2 || professional.nome.length > 100) 
+            errors.push("Nome deve ter entre 2 e 100 caracteres.");
+
+        // VALIDAÇÕES DE TELEFONE
+        if (!validatePhone(professional.telefone))
+            errors.push("Formato de telefone inválido - o formato deve ser (xx) xxxxx-xxxx ou (xx) xxxx-xxxx.");
+
+        // FAZER: Verificação da existência da especialidade_id no banco de dados
+        // VALIDAÇÕES DE ESPECIALIDADE
+        if (
+            typeof professional.especialidade_id !== "number" || 
+            !Number.isInteger(professional.especialidade_id)  ||
+            professional.especialidade_id <= 0
+        ) 
+            errors.push("Especialidade com formato inválido.");
+        
+        if (errors.length > 0) { 
+            errors[0] = "FALHA NA VALIDAÇÃO DO PROFISSIONAL: " + errors[0]; // Prefixa a primeira mensagem de erro
+            throw new Error(errors.join(" ")); // Lança um erro com todas as mensagens de validação
+        }
+
+    }
+
     // Busca todos os profissionais cadastrados
     static async getAllProfessionals() {
         return await ProfessionalModel.findAll();
@@ -15,6 +62,8 @@ class ProfessionalService {
             // professionalModel.js --> findSpecialtyById(professional.especialidade_id)
             // specialtyModel.js --> findById(professional.especialidade_id)
 
+        this.validateProfessional(professional); // Chama a função de validação
+
         return await ProfessionalModel.create(professional); // Cria o novo profissional
     }
 
@@ -23,7 +72,9 @@ class ProfessionalService {
         // FAZER: validar a especialidade do profissional
             // professionalModel.js --> findSpecialtyById(professional.especialidade_id)
             // specialtyModel.js --> findById(professional.especialidade_id)
-            
+
+        this.validateProfessional(professional); // Chama a função de validação
+
         const updatedRows = await ProfessionalModel.update(id, professional);
         if (updatedRows === 0) {
             throw new Error("Profissional não encontrado."); // Caso nenhum profissional tenha sido atualizado
